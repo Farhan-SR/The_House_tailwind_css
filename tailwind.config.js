95cf76147aa962a4b0e12c9af0f0331763c73928
Alien <farhan10670@gmail.com>
2024-08-24 01:34:36 +0600

    chang added

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          //sans: ['Manrope', 'sans-serif'], // Adds Manrope as the default sans font
+          italianno: ['Italianno', 'cursive'],
+        },
+      },
+    },
+    plugins: [],
+  }
+  
